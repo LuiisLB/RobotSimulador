9c65be69d9991f6c85e09b0d44dc411ddc4fab9b
Jose Luis Bautista <70413431+LuiisLB@users.noreply.github.com>
2022-06-30 16:28:41 -0500

    SubidaFinal
    Entrega final con menu e interacion dentro

--- Assets/Scripts/MenuOpciones.cs
+++ Assets/Scripts/MenuOpciones.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MenuOpciones : MonoBehaviour
 {
+    
     // Start is called before the first frame update
     public void Jugar()
     {
--- Assets/Scripts/TomarObjeto.cs
+++ Assets/Scripts/TomarObjeto.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TomarObjeto : MonoBehaviour
 {
     public GameObject ObjectToPickup;
     public GameObject pickedObject;
     public Transform InteractionZone;
+   
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            
+                SceneManager.LoadScene("MenuPrincipal");
+            
+        }
         if(ObjectToPickup!=null&&ObjectToPickup.GetComponent<Agarraobjeto>().sepuedetomar==true&&pickedObject==null)
         {
             if(Input.GetKeyDown(KeyCode.F))
