5abfff76720e9ee55700cb32510badc607eb4655
Jose Luis Bautista <70413431+LuiisLB@users.noreply.github.com>
2022-06-29 22:09:08 -0500

    Creacion Menu

---
+++ Assets/Scripts/MenuOpciones.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuOpciones : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Jugar()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    // Update is called once per frame
+    public void Salir()
+    {
+        Application.Quit();
+    }
+}
